adaf5757411f04e4dd68b633795decde3c7bacfe
daehee.kim <p.daehee@kalmate.net>
2022-01-27 23:23:19 +0900

    update

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Grid, Tabs, Tab, TextField, Box, Button } from '@mui/material';
+import { Grid, Tabs, Tab, TextField, Box, Button, Typography, IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import TabPanel from './TabPane';
 import TagChipInputText from './TagChipInputText';
@@ -19,6 +19,7 @@ const md = markdown(opts);
 
 function EditorPane() {
   const dispatch = useDispatch();
+  const [hasError, setError] = useState(false);
   const [index, setIndex] = useState(0);
   const [text, setText] = useState();
   const [preview, setPreview] = useState();
@@ -29,13 +30,26 @@ function EditorPane() {
     setTags([]);
     setText(null);
     setPreview(null);
+    setError(false);
   };
 
   const markup = () => {
-    return { __html: preview };
+    if (preview && preview.length > 0) {
+      return { __html: preview };
+
+    }
+
+    return null;
+
   };
 
   const saveToServer = () => {
+    if (!text || text.trim().length == 0) {
+
+      setError(true);
+      return;
+    }
+
     const payload = {
       sub: text.split(/\n/)[0],
       body: text,
@@ -53,19 +67,22 @@ function EditorPane() {
   }, [index]);
 
   return (
-    <Box sx={{ width: 550 }}>
-      <Tabs value={index} onChange={(_, value) => setIndex(value)}>
-        <Tab label="Source" />
-        <Tab label="Preview" />
-      </Tabs>
+    <>
+      <Box sx={{ display: 'flex', width: 550 }}>
+        <Tabs value={index} onChange={(_, value) => setIndex(value)} sx={{ flexGrow: 1 }}>
+          <Tab label="Source" />
+          <Tab label="Preview" />
+        </Tabs>
 
-      <Button size="small" onClick={closeEditor}>
-        <CloseIcon />
-      </Button>
+        <IconButton size="small" onClick={closeEditor}>
+          <CloseIcon />
+        </IconButton>
+      </Box>
 
       <TabPanel value={index} index={0} style={{ padding: 4 }}>
         <Box style={{ paddingBottom: 5 }}>
           <TextField
+            error={hasError}
             fullWidth
             multiline
             value={text}
@@ -87,7 +104,7 @@ function EditorPane() {
           Save
         </Button>
       )}
-    </Box>
+    </>
   );
 }
 
